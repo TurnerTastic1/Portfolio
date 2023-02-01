7803eaf5459cae37d45bec9d8eeb20fa803c150f
Turner Naef <Turner.Naef@gmail.com>
2023-01-31 23:39:29 -0700

    testing new azure stuff

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 function MissingPage() {
+  function SendHome() {
+    window.location.href = "/home";
+  }
+	
   return (
     <div>
       <p>404!</p>
+      <button onClick={SendHome}> Take me home </button>
     </div>
   )
 }
