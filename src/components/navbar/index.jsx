6c1f2491e3e50804384140fa9d1e5f22911a168c
Turner Naef <Turner.Naef@gmail.com>
2023-02-02 08:23:50 -0700

    Commiting Navbar before more azure tests

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+// get styled components
 
 // Comment for testing linter for a new branch
 
 function Navbar() {
   return (
-    <div>
-      <p>This is the navbar!</p>
-    </div>
+    <nav>
+      <div>
+        <p>This is the navbar!</p>
+      </div>
+    </nav>
   )
 }
 
