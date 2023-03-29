b8030d83ec3b2800531f64207b6869abe88a0aef
Turner Naef <Turner.Naef@gmail.com>
2023-03-28 20:25:34 -0400

    Lint changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 
-// Components
-import Navbar from "./components/navbar/index.jsx";
-import Nav from "./components/Nav/index.jsx";
-
 // Page imports
 import Site from "./pages/site.jsx";
 import NotFound from "./pages/404.jsx";
-
-import ParticlesComponent from "./components/particles/stars.jsx";
 
 const AppContainer = styled.div`
   width: 100%;
